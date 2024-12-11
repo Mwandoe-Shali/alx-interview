47f388807dd5839cce95f064796aacd8ad117d03
Mwandoe-Shali <mwandoeshali@gmail.com>
2024-12-11 22:32:34 +0300

    edited for the pep8 validation

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -9,7 +9,7 @@ def island_perimeter(grid):
     Calculate the perimeter of the island described in grid.
 
     Args:
-        grid (list of list of int): 2D grid where 1 represents land and 0 represents water.
+        grid (list of list of int): 2D grid 1 = land and 0 = water.
 
     Returns:
         int: The perimeter of the island.
@@ -28,22 +28,22 @@ def island_perimeter(grid):
             # Check if the current cell is land (1)
             if grid[i][j] == 1:
                 # Check the cell above (up)
-                # If it's the first row or the cell above is water (0), add to perimeter
+                # If first row or cell above is water (0), add to perimeter
                 if i == 0 or grid[i - 1][j] == 0:
                     perimeter += 1
 
                 # Check the cell below (down)
-                # If it's the last row or the cell below is water (0), add to perimeter
+                # If last row or cell below is water (0), add to perimeter
                 if i == rows - 1 or grid[i + 1][j] == 0:
                     perimeter += 1
 
                 # Check the cell to the left
-                # If it's the first column or the cell to the left is water (0), add to perimeter
+                # If first col or cell to left = water (0), add to perimeter
                 if j == 0 or grid[i][j - 1] == 0:
                     perimeter += 1
 
                 # Check the cell to the right
-                # If it's the last column or the cell to the right is water (0), add to perimeter
+                # If last col or cell to right = water (0), add to perimeter
                 if j == cols - 1 or grid[i][j + 1] == 0:
                     perimeter += 1
 
